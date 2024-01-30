9faba0008f8b41c89a141170b7b1d1cc57a730a4
JavaCommons Technologies <javacommons@gmail.com>
2024-01-31 07:59:45 +0900

    JsonDLL v2024.0131.0759.39

--- JsonDLL/JsonAPI.cs
+++ JsonDLL/JsonAPI.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -16,8 +17,20 @@ public class JsonAPI
     }
     public JsonAPI(string dllName)
     {
+        //Tool.Print(dllName, "dllName");
         // for client
-        this.handle = LoadLibraryW(dllName);
+        if (System.IO.Path.IsPathRooted(dllName))
+        {
+            this.handle = LoadLibraryExW(
+                dllName,
+                IntPtr.Zero,
+                LoadLibraryFlags.LOAD_WITH_ALTERED_SEARCH_PATH
+                );
+        }
+        else
+        {
+            this.handle = LoadLibraryW(dllName);
+        }
         this.funcPtr = GetProcAddress(handle, "Call");
     }
     public dynamic Call(dynamic name, dynamic args)
@@ -61,6 +74,21 @@ public class JsonAPI
     }
     [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
     internal static extern IntPtr LoadLibraryW(string lpFileName);
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    public static extern IntPtr LoadLibraryExW(string dllToLoad, IntPtr hFile, LoadLibraryFlags flags);
     [DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = false)]
     internal static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+    [System.Flags]
+    public enum LoadLibraryFlags : uint
+    {
+        DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+        LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+        LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+        LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+        LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+        LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008,
+        LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+        LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+        LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000
+    }
 }
